f43ee3ed5a6723e3f8a7ce8a06ea8ce567e41eb2
indev-aj <amrin.wan@gmail.com>
2020-10-24 07:10:09 +0800

    Add project files.

---
+++ QMS Monaliza/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace QMS_Monaliza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.orderLbl = new System.Windows.Forms.Label();
+            this.phoneTxt = new System.Windows.Forms.TextBox();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.currentOrderLbl = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.nextOrderLbl3 = new System.Windows.Forms.Label();
+            this.nextOrderLbl2 = new System.Windows.Forms.Label();
+            this.nextOrderLbl1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.submitBtn);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.orderLbl);
+            this.panel1.Controls.Add(this.phoneTxt);
+            this.panel1.Controls.Add(this.nameTxt);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(393, 409);
+            this.panel1.TabIndex = 0;
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.AutoSize = true;
+            this.submitBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitBtn.Location = new System.Drawing.Point(133, 345);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(129, 44);
+            this.submitBtn.TabIndex = 7;
+            this.submitBtn.Text = "SUBMIT";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            this.submitBtn.Click += new System.EventHandler(this.submitBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(40, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(337, 46);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "ORDER NUMBER";
+            // 
+            // orderLbl
+            // 
+            this.orderLbl.AutoSize = true;
+            this.orderLbl.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.orderLbl.Location = new System.Drawing.Point(157, 282);
+            this.orderLbl.Name = "orderLbl";
+            this.orderLbl.Size = new System.Drawing.Size(80, 46);
+            this.orderLbl.TabIndex = 5;
+            this.orderLbl.Text = "106";
+            // 
+            // phoneTxt
+            // 
+            this.phoneTxt.Location = new System.Drawing.Point(98, 148);
+            this.phoneTxt.Name = "phoneTxt";
+            this.phoneTxt.Size = new System.Drawing.Size(279, 22);
+            this.phoneTxt.TabIndex = 4;
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(98, 115);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(279, 22);
+            this.nameTxt.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Phone";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRATION";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.currentOrderLbl);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(393, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(407, 180);
+            this.panel2.TabIndex = 1;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(9, 41);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(386, 46);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "CURRENT NUMBER";
+            // 
+            // currentOrderLbl
+            // 
+            this.currentOrderLbl.AutoSize = true;
+            this.currentOrderLbl.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentOrderLbl.Location = new System.Drawing.Point(146, 115);
+            this.currentOrderLbl.Name = "currentOrderLbl";
+            this.currentOrderLbl.Size = new System.Drawing.Size(80, 46);
+            this.currentOrderLbl.TabIndex = 6;
+            this.currentOrderLbl.Text = "102";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.nextBtn);
+            this.panel3.Controls.Add(this.nextOrderLbl3);
+            this.panel3.Controls.Add(this.nextOrderLbl2);
+            this.panel3.Controls.Add(this.nextOrderLbl1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(393, 186);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(407, 223);
+            this.panel3.TabIndex = 2;
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.AutoSize = true;
+            this.nextBtn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextBtn.Location = new System.Drawing.Point(128, 159);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(129, 44);
+            this.nextBtn.TabIndex = 8;
+            this.nextBtn.Text = "NEXT";
+            this.nextBtn.UseVisualStyleBackColor = true;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // nextOrderLbl3
+            // 
+            this.nextOrderLbl3.AutoSize = true;
+            this.nextOrderLbl3.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextOrderLbl3.Location = new System.Drawing.Point(146, 96);
+            this.nextOrderLbl3.Name = "nextOrderLbl3";
+            this.nextOrderLbl3.Size = new System.Drawing.Size(80, 46);
+            this.nextOrderLbl3.TabIndex = 10;
+            this.nextOrderLbl3.Text = "105";
+            // 
+            // nextOrderLbl2
+            // 
+            this.nextOrderLbl2.AutoSize = true;
+            this.nextOrderLbl2.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextOrderLbl2.Location = new System.Drawing.Point(146, 50);
+            this.nextOrderLbl2.Name = "nextOrderLbl2";
+            this.nextOrderLbl2.Size = new System.Drawing.Size(80, 46);
+            this.nextOrderLbl2.TabIndex = 8;
+            this.nextOrderLbl2.Text = "104";
+            // 
+            // nextOrderLbl1
+            // 
+            this.nextOrderLbl1.AutoSize = true;
+            this.nextOrderLbl1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextOrderLbl1.Location = new System.Drawing.Point(146, 4);
+            this.nextOrderLbl1.Name = "nextOrderLbl1";
+            this.nextOrderLbl1.Size = new System.Drawing.Size(80, 46);
+            this.nextOrderLbl1.TabIndex = 9;
+            this.nextOrderLbl1.Text = "103";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 409);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "QMS Monaliza";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox phoneTxt;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label orderLbl;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button submitBtn;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label currentOrderLbl;
+        private System.Windows.Forms.Label nextOrderLbl3;
+        private System.Windows.Forms.Label nextOrderLbl2;
+        private System.Windows.Forms.Label nextOrderLbl1;
+        private System.Windows.Forms.Button nextBtn;
+    }
+}
+
---
+++ QMS Monaliza/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QMS_Monaliza
+{
+    public partial class Form1 : Form
+    {
+        int currentOrderNumber;
+        int nextOrderNumber;
+        int orderNumber;
+
+        public Form1()
+        {
+            InitializeComponent();
+            currentOrderNumber = 0;
+            nextOrderNumber = 0;
+            orderNumber = 0;
+
+            orderLbl.Text = "";
+            currentOrderLbl.Text = "";
+            nextOrderLbl1.Text = "";
+            nextOrderLbl2.Text = "";
+            nextOrderLbl3.Text = "";
+        }
+
+        private void submitBtn_Click(object sender, EventArgs e)
+        {
+            String name = nameTxt.Text;
+            String phone = phoneTxt.Text;
+
+            orderNumber += 1;
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            currentOrderNumber += 1;
+            nextOrderNumber = currentOrderNumber + 1;
+            currentOrderLbl.Text = currentOrderNumber.ToString();
+            nextOrderLbl1.Text = nextOrderNumber.ToString();
+            nextOrderLbl2.Text = (nextOrderNumber + 1).ToString();
+            nextOrderLbl3.Text = (nextOrderNumber + 2).ToString();
+        }
+    }
+}
